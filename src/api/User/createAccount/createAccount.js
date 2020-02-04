77e0113c436ae31c962611c27e3eca5bafca2ac8
Jungbumwoo <jungbumwoo@Naver.com>
2020-02-04 15:01:39 +0900

    0204

---
+++ src/api/User/createAccount/createAccount.js
@@ -0,0 +1,10 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Mutation: {
+        createAccount: async(_, args) =>{
+            const { email, keeplist } = args;
+            const user = await prisma.createUser({ email, keeplist});
+        }
+    }
+}
